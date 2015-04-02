f3fe3e4ba9e29cd5a58b5744bfb2d3e7ec75292d
whosesmile <lovemoon@yeah.net>
2015-04-02 18:26:34 +0800

    change sender to noreply

--- app/config/mail.js
+++ app/config/mail.js
@@ -1,30 +1,30 @@
 var nodemailer = require('nodemailer');
 
 // 发件人
-var email = {
+var sender = {
   nick: '李双宝',
-  user: 'smilelegend@smilelegend.com',
-  pass: 'idontknow123456'
+  user: 'noreply@smilelegend.com',
+  pass: 'fuckpassword8'
 };
 
 // 收件人列表
-var receivers = ['lovemoon@yeah.net', 'whosesmile@gmail.com'];
+var receivers = ['lovemoon@yeah.net', 'whosesmile@gmail.com', 'smilelegend@smilelegend.com'];
 
 // 构建
 var transporter = nodemailer.createTransport({
   host: 'smtp.exmail.qq.com',
   secure: true,
-  auth: email
+  auth: sender
 });
 
 // 提供接口
 exports.sendMail = function (title, message) {
   return new Promise(function (resolve, reject) {
     transporter.sendMail({
-      from: email.nick + '<' + email.user + '>', // sender address
+      from: sender.nick + '<' + sender.user + '>', // sender address
       bcc: receivers,
-      subject: title || '测试邮件',
-      html: message || 'Just a testing~'
+      subject: title,
+      html: message
     }, function (error, info) {
       if (error) {
         console.log(error);
