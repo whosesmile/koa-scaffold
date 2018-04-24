ccf54fcd2ade6fef206f02642964948821b6b553
whosesmile <whosesmile@gmail.com>
2018-04-24 16:02:28 +0800

    fix some trick

--- src/middleware/send.ts
+++ src/middleware/send.ts
@@ -7,12 +7,12 @@ const options = {
 };
 
 // 防止每次重复定义的性能损失
-const REGEX_PREFIX = new RegExp('^' + process.env.STATIC_PREFIX);
+const REG_PREFIX = new RegExp('^' + process.env.STATIC_PREFIX);
 
 export default function (ctx: Context, next: () => Promise<any>) {
   if (ctx.path.startsWith(process.env.STATIC_PREFIX!)) {
     // prefix可变, 但按约定静态目录为static文件夹内, 所以此处做替换
-    const path = ctx.path.replace(REGEX_PREFIX, '/static/');
+    const path = ctx.path.replace(REG_PREFIX, '/static/');
     return send(ctx, path, options);
   }
   return next();
